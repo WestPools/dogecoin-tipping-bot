43251594373cf60c0d46ce15929afd527bb33100
BTShare <35891486+west-pool@users.noreply.github.com>
2018-03-06 13:49:54 +0000

    Update messages.js

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,4 @@
 module.exports = {
-  OOPS_TEXT: 'Oops ! Something went wrong. Contact Lola.',
+  OOPS_TEXT: 'Oops ! Something went wrong. Contact ddvs1.',
   QRCODE_TEXT: 'Hey people, send me much money ! Love you <3'
 }
